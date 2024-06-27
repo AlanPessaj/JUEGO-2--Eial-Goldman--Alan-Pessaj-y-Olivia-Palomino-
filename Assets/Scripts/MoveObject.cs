daec6ee84b87ae32db022d544177adbdbe0895c1
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-06-27 09:10:59 -0300

    mover

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    Vector3 position1;
+    Vector3 position2;
+
+    Vector3 ogPosition1;
+    Vector3 ogPosition2;
+    public controller myController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void moveObject()
+    {
+        ogPosition1 = controller.selected1.transform.position;
+        ogPosition2 = controller.selected2.transform.position;
+        for (float i = 0; i < 1; i+= 0.1f)
+        {
+            controller.selected1.transform.position = Mathf.Lerp(ogPosition1, position1, i);
+            controller.selected2.transform.position = Mathf.Lerp(-ogPosition2, position2, i);
+        }
+    }
+}
