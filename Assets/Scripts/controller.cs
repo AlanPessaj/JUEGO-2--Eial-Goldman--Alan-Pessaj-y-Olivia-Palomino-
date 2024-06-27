3a1ba078598db29747cf5aa0932febe0178ecef6
PESSAJ ALAN <48242487@ort>
2024-06-27 08:21:24 -0300

    Array

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class config : MonoBehaviour
+public class controller : MonoBehaviour
 {
+    public GameObject[] Productos;
     // Start is called before the first frame update
     void Start()
     {
