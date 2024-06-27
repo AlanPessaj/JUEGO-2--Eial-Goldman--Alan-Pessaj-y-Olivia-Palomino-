4ec457a75f213d0c82ff0d596cd730283d0817e2
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-06-27 09:30:45 -0300

    fix

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -10,10 +10,14 @@ public class MoveObject : MonoBehaviour
     Vector3 ogPosition1;
     Vector3 ogPosition2;
     public controller myController;
+
+    GameObject object1;
+    GameObject object2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        object1 = Instantiate(controller.selected1);
+        object2 = Instantiate(controller.selected2);
     }
 
     // Update is called once per frame
@@ -24,12 +28,20 @@ public class MoveObject : MonoBehaviour
 
     public void moveObject()
     {
-        ogPosition1 = controller.selected1.transform.position;
-        ogPosition2 = controller.selected2.transform.position;
+        ogPosition1 = object1.transform.position;
+        ogPosition2 = object2.transform.position;
         for (float i = 0; i < 1; i+= 0.1f)
         {
-            /*controller.selected1.transform.position = Mathf.Lerp(ogPosition1, position1, i);
-            controller.selected2.transform.position = Mathf.Lerp(-ogPosition2, position2, i);*/
+            Vector3 temp1;
+            Vector3 temp2;
+            temp1 = new Vector3(Mathf.Lerp(ogPosition1.x, position1.x, i), 0, 0);
+            temp2 = new Vector3(Mathf.Lerp(ogPosition2.x, position2.x, i), 0, 0);
+
+            temp1 = new Vector3(temp1.x, Mathf.Lerp(ogPosition1.y, position1.y, i), 0);
+            temp2 = new Vector3(temp2.x, Mathf.Lerp(ogPosition2.y, position2.y, i), 0);
+
+            temp1 = new Vector3(temp1.x, temp1.y, Mathf.Lerp(ogPosition1.x, position1.x, i));
+            temp2 = new Vector3(temp2.x, temp2.y, Mathf.Lerp(ogPosition2.x, position2.x, i));
         }
     }
 }
